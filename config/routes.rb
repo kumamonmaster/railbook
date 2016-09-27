32a0b0f7f80e233abd258963aeea94ae10ef5775
kumamonmaster <kumamonmaster@gmail.com>
2016-09-27 09:57:48 +0000

    Add some models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :fan_comments
+  resources :reviews
+  resources :authors
+  resources :users
   root 'hello#index'
+  resources :books
   get '/view' => 'hello#view'
   get '/list' => 'hello#list'
 end
